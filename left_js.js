9f987da8f466759c70ef34b825eee936d5473197
sukhsung@umich.edu <Tjrgus93>
2020-03-13 15:21:01 -0400

    removed outdated

--- left_js.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var $left = $(".left")
-    var $right = $(".right")
-
-    $left.css("background-color",localStorage.getItem("leftColor"));
-    $right.css("background-color",localStorage.getItem("rightColor"));
-
-});
-
