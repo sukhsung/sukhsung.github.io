5b4d50567564fa1d8ad439643ecf36a1bdef72a4
sukhsung <sukhsung@umich.edu>
2020-03-13 20:47:29 -0400

    added some transition animations

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 function loadContent(pageName){
     $(".active").removeClass("active").addClass("inactive");
     $("#"+pageName).removeClass("inactive").addClass("active");
-    $("#content").load(pageName+".html");
+    $("#content").load(pageName+".html").hide().fadeIn();
 }
 
 function loadMain() {
     $(".active").removeClass("active").addClass("inactive");
-    $("#content").load("main.html");
+    $("#content").load("main.html").hide().fadeIn();
 }
