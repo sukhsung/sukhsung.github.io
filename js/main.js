99e74a0be2564bf1828183aa394a0502917de577
sukhsung <sukhsung@umich.edu>
2020-03-15 22:01:45 -0400

    added new color scheme

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+
+
 function loadContent(pageName){
     $(".active").removeClass("active");
     $("#"+pageName).addClass("active");
@@ -8,8 +10,20 @@ function loadContent(pageName){
 function loadMain() {
     $(".active").removeClass("active");
     $("#content").load("main.html").hide().show().css({top:'1em',opacity:0}).animate({top:0,opacity:1});
+
+    color_index = (color_index+1)%2;
+    console.log(color_index);
+    document.documentElement.style.setProperty('--color-main', color_main[color_index]);
+    document.documentElement.style.setProperty('--color-light', color_light[color_index]);
+    document.documentElement.style.setProperty('--color-accent', color_accent[color_index]);
+    document.documentElement.style.setProperty('--color-bkg', color_bkg[color_index]);
 }
 
-function testFunc(){
-    $("#content")
+function loadVar(){
+
+    color_main= ["#022873","#F2BC8D"];
+    color_light= ["#46658C","#E3B46D"];
+    color_accent= ["#FB5569","#FB5569"];
+    color_bkg= ["#F2E1D8","#383D59"];
+    color_index= 0;
 }
