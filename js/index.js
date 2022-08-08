f9ff82d1938e65a4d61a1d9a2e7762003d29a40c
Suk Hyun Sung <sukhsung@umich.edu>
2022-08-08 18:03:28 -0400

    publication now managed with js

--- js/index.js
+++ js/index.js
@@ -40,23 +40,13 @@ function loadVar(){
     color_light= ["#46658C","#E3B46D"];
     color_accent= ["#FB5569","#FB5569"];
     color_bkg= ["#F2E1D8","#383D59"];
-    darkmode_icon = ["lightmode.svg","darkmode.svg"]
-    // d = new Date();
-    // h = d.getHours();
-    // if (h <=7 || h>19){
-    //     color_index = 0;
-    // }
-    // else{
-    //     color_index = 1;
-    // }
-    // console.log('hi')
+    darkmode_icon = ["/data/lightmode.svg","/data/darkmode.svg"]
+
+    // detect dark mode
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        // console.log('dark')// dark mode
         color_index = 0;
     }
     else {
-        // console.log('day')
         color_index = 1;
-
     }
 }
---
+++ js/publications.js
@@ -0,0 +1,54 @@
+//var mydata = JSON.parse(pubList);
+
+
+/* <li><a href="https://doi.org/10.1038/ncomms5530">Atomically precise interfaces from stoichiometric deposition<br /><span class="gray"><i>Nature Communications,</i> 2014</span></a></li> */
+
+                  
+var container = document.getElementById( "testdiv" )
+
+    
+    
+    
+    
+    
+
+for (i=0; i<pubList.length; i++) {
+    // make title
+    var title = pubList[i].title
+    
+    // make newline element
+    var br = document.createElement('br')
+
+    // make info
+    var span = document.createElement('span')
+
+    var journal = document.createElement('i')
+    journal.appendChild( document.createTextNode(pubList[i].journal))
+
+    var year = document.createTextNode( ", " + pubList[i].year)
+
+    span.appendChild( journal)
+    span.appendChild( year )
+    
+    span.classList.add("gray")
+
+    var li = document.createElement('li')
+    // Create anchor element.
+    var anchor = document.createElement('a'); 
+    // Create the text node for anchor element.
+    var link = document.createTextNode(title);
+    
+    // Append the text node to anchor element.
+    anchor.appendChild(link); 
+    // Set the href property.
+    anchor.href = pubList[i].url; 
+    // Set the title.
+    anchor.title = pubList[i].title 
+    // Append the anchor element to the body.
+    li.appendChild(anchor)
+    li.appendChild(br)
+    li.append(span)
+    container.appendChild(li); 
+    
+    console.log( pubList[i].title )
+}
